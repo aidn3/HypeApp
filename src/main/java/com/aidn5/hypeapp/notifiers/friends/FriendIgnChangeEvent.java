a7173c1bc712ac62ff7945078a69b9d118896473
aidn3 <aidn.david3@gmail.com>
2019-02-25 18:53:49 +0100

    Split the data to make the look up faster by multi-threading
    requesting data from the internet by speed of 5 kb/s (2 requests per second)
    is too slow and takes lot of time for array bigger than 50
    Using multi-threading can speed up the process
    ThreadPoolExecutor is required
    since we don't need to overwhelm the system
    with too many threads running at the same time
    also, This is why the thread ThreadPoolExecutor has maximumPoolSize of 5

--- src/main/java/com/aidn5/hypeapp/notifiers/friends/FriendIgnChangeEvent.java
+++ src/main/java/com/aidn5/hypeapp/notifiers/friends/FriendIgnChangeEvent.java
@@ -2,7 +2,9 @@ package com.aidn5.hypeapp.notifiers.friends;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.aidn5.hypeapp.R;
 import com.aidn5.hypeapp.hypixelapi.FriendsRequest;
@@ -12,7 +14,14 @@ import com.aidn5.hypeapp.services.IgnProvider;
 import com.aidn5.hypeapp.services.Settings;
 import com.snappydb.DB;
 
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public final class FriendIgnChangeEvent extends NotifierFactory {
+	private static final int CHUNK_SIZE = 5; //How many players does every thread has to work with
+
 	public FriendIgnChangeEvent(Context context, DB db, IgnProvider ignProvider, SharedPreferences settings) {
 		super(context, db, ignProvider, settings);
 	}
@@ -22,15 +31,49 @@ public final class FriendIgnChangeEvent extends NotifierFactory {
 	 */
 	@Override
 	public void doLoop() {
-		String[] playersUUID = getPlayersUUID();
-		if (playersUUID == null) return; //No-data -> nothing to do -> return
-
 		boolean showNotification = settings.getBoolean(Settings.showNotificationOnFriendIgnChanged.name(), true);
 
 		// We don't need to look up the usernames anymore,
 		// since we won't notify the user anyways
 		if (!showNotification) return;
 
+		String[] playersUUID = getPlayersUUID();
+		if (playersUUID == null) return; //No-data -> nothing to do -> return
+
+		// Split the data to make the look up faster by multi-threading
+		// requesting data from the internet by speed of 5 kb/s (2 requests per second)
+		// is too slow and takes lot of time for array bigger than 50
+		// Using multi-threading can speed up the process
+		// ThreadPoolExecutor is required
+		// since we don't need to overwhelm the system
+		// with too many threads running at the same time
+		// also, This is why the thread ThreadPoolExecutor has maximumPoolSize of 5
+
+		String[][] playersUUIDChunk = splitArray(playersUUID, CHUNK_SIZE);
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(playersUUIDChunk.length + 1));
+
+		for (String[] chunk : playersUUIDChunk) {
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					lookUpUUIDs(chunk);
+				}
+			});
+		}
+
+		while (executor.getTaskCount() != executor.getCompletedTaskCount()) {
+			Log.v(getClass().getSimpleName(), "count=" + executor.getTaskCount() + "," + executor.getCompletedTaskCount());
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ignored) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		executor.shutdown();
+	}
+
+	private void lookUpUUIDs(String[] playersUUID) {
 		for (String uuid : playersUUID) {
 			ignProvider.cleanDB(); // clean the database to make the look-up faster
 
@@ -49,6 +92,55 @@ public final class FriendIgnChangeEvent extends NotifierFactory {
 				);
 			}
 		}
+	}
+
+	/**
+	 * Split array to smaller arrays
+	 * <p>
+	 * <b>example to chunkSize:</b>
+	 * <p>
+	 * chunkSize = 3
+	 * [1, 2, 3]
+	 * [4, 5]
+	 * <p>
+	 * chunkSize = 4
+	 * [1, 2, 3, 4]
+	 * [5]
+	 * <p>
+	 * chunkSize = 5
+	 * [1, 2, 3, 4, 5]
+	 * <p>
+	 * <p>
+	 * https://stackoverflow.com/questions/27857011/how-to-split-a-string-array-into-small-chunk-arrays-in-java
+	 *
+	 * @param arrayToSplit the array to split
+	 * @param chunkSize    how many items does every array has to have at max
+	 * @return arrays of smaller arrays of the original array
+	 */
+	@NonNull
+	private String[][] splitArray(@NonNull String[] arrayToSplit, int chunkSize) {
+		chunkSize = Math.abs(chunkSize);
+
+		// first we have to check if the array can be split in multiple
+		// arrays of equal 'chunk' size
+		int rest = arrayToSplit.length % chunkSize;  // if rest>0 then our last array will have less elements than the others
+		// then we check in how many arrays we can split our input array
+		int chunks = arrayToSplit.length / chunkSize + (rest > 0 ? 1 : 0); // we may have to add an additional array for the 'rest'
+		// now we know how many arrays we need and create our result array
+		String[][] arrays = new String[chunks][];
+		// we create our resulting arrays by copying the corresponding
+		// part from the input array. If we have a rest (rest>0), then
+		// the last array will have less elements than the others. This
+		// needs to be handled separately, so we iterate 1 times less.
+		for (int i = 0; i < (rest > 0 ? chunks - 1 : chunks); i++) {
+			// this copies 'chunk' times 'chunkSize' elements into a new array
+			arrays[i] = Arrays.copyOfRange(arrayToSplit, i * chunkSize, i * chunkSize + chunkSize);
+		}
+		if (rest > 0) { // only when we have a rest
+			// we copy the remaining elements into the last chunk
+			arrays[chunks - 1] = Arrays.copyOfRange(arrayToSplit, (chunks - 1) * chunkSize, (chunks - 1) * chunkSize + rest);
+		}
+		return arrays; // that's it
 	}
 
 	/**
