d48117662dc93b930d102a17396c8d611dfc93cb
aidn3 <aidn.david3@gmail.com>
2019-02-26 20:33:57 +0100

    Recreating the main gui. Adding friends and guild tab support

--- src/main/java/com/aidn5/hypeapp/activities/main_activity/BaseFragment.java
+++ src/main/java/com/aidn5/hypeapp/activities/main_activity/BaseFragment.java
@@ -9,33 +9,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.aidn5.hypeapp.R;
 
 import java.util.Objects;
 
 abstract class BaseFragment extends Fragment {
-	private final static byte TAP_TO_LOAD = 1;
-	private final static byte LOADING = 2;
-	private final static byte LOADED = 3;
+	protected final static byte PENDING = 1;
+	protected final static byte LOADING = 2;
+	protected final static byte LOADED = 3;
+	protected final static byte FAILED = 4;
 
-	protected BaseAdapter listAdapter;
-
-	private byte action = 0;
+	protected BaseAdapter adapter;
+	private byte action = PENDING;
 
 	@Override
 	public final View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		if (autoLoad(getContext())) {
-			refresh();
+			new Thread(this::refresh).start();
 			return null;
 		}
 
-		if (action == TAP_TO_LOAD)
-			return getTapToLoadView(inflater, container);
-		else if (action == LOADING)
-			return getLoadingView(inflater, container);
-
-		return getTapToLoadView(inflater, container);
+		switch (action) {
+			case PENDING:
+				return onTapToLoadEvent(inflater, container);
+			case LOADING:
+				return onLoadingEvent(inflater, container);
+			case LOADED:
+				return onLoadedEvent(inflater, container);
+			case FAILED:
+				return null;
+			default:
+				return onCustomEvent(inflater, container);
+		}
 	}
 
 	protected final synchronized void setState(byte action) {
@@ -49,25 +56,35 @@ abstract class BaseFragment extends Fragment {
 		}
 	}
 
-	private View getTapToLoadView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+	protected View onTapToLoadEvent(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
 		View view = inflater.inflate(R.layout.main_player_list_tap_to_load, container, false);
 
 		view.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				refresh();
+				new Thread(BaseFragment.this::refresh).start();
 			}
 		});
 		return view;
 	}
 
-	private View getLoadingView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+	protected View onLoadingEvent(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
 		return inflater.inflate(R.layout.loading_progress, container, false);
 	}
 
-	protected abstract CharSequence getTitle(@NonNull Context context);
+	protected View onLoadedEvent(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+		View view = inflater.inflate(R.layout.main_player_list, container, false);
 
-	protected abstract void refresh();
+		ListView listView = view.findViewById(R.id.main_playerList_list_listView);
+		listView.setAdapter(adapter);
+
+		return view;
+	}
+
+	protected View onCustomEvent(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+		//Override me!
+		return null;
+	}
 
 	@Nullable
 	@Override
@@ -75,7 +92,12 @@ abstract class BaseFragment extends Fragment {
 		return super.getContext() != null ? super.getContext() : super.getActivity();
 	}
 
-	protected boolean autoLoad(@NonNull Context context) {
+	protected boolean autoLoad(@Nullable Context context) {
+		if (context == null) return false;
 		return false;
 	}
+
+	protected abstract CharSequence getTitle(@NonNull Context context);
+
+	protected abstract void refresh();
 }
--- src/main/java/com/aidn5/hypeapp/activities/main_activity/FriendsFragment.java
+++ src/main/java/com/aidn5/hypeapp/activities/main_activity/FriendsFragment.java
@@ -1,25 +1,28 @@
 package com.aidn5.hypeapp.activities.main_activity;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aidn5.hypeapp.R;
 import com.aidn5.hypeapp.hypixelapi.FriendsRequest;
 import com.aidn5.hypeapp.hypixelapi.HypixelReplay;
+import com.aidn5.hypeapp.services.IgnProvider;
+import com.squareup.picasso.Picasso;
 
-//TODO: [code] Fix FriendsFragment
-public class FriendsFragment extends BaseFragment {
-	@Override
-	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-	}
+public final class FriendsFragment extends BaseFragment {
 
 	@Override
 	public CharSequence getTitle(@NonNull Context context) {
@@ -28,33 +31,119 @@ public class FriendsFragment extends BaseFragment {
 
 	@Override
 	public synchronized void refresh() {
+		setState(LOADING); // show the loading indicator
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				HypixelReplay friendsRequest = new FriendsRequest(
-						getContext()
-				).getFriendsByUserUUID(
-						//G.getSettings(getContext())
-						"asd", "asd"
-				);
+		Context context = getContext();
+		if (context == null) {
+			setState(FAILED);
+			return;
+		}
 
-				if (!friendsRequest.isSuccess)
-					throw new RuntimeException(friendsRequest.exception);
+		// Load data
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+		HypixelReplay friendsRequest = new FriendsRequest(context).getFriendsByUserUUID(settings);
 
+		String[] friends = (String[]) friendsRequest.value;
 
-				ListView listView = getView().findViewById(R.id.main_playerList_list_listView);
+		// Check on errors
+		if (!friendsRequest.isSuccess || friends == null) {
+			setState(FAILED);
+			return;
+		}
 
-				String[] friends = (String[]) friendsRequest.value;
-				Log.v("MEEE", friends[0]);
-			}
-		}).start();
+		// Create the adapter and set it
+		this.adapter = new Adapter(
+				getLayoutInflater(), context, new IgnProvider(context),
+				friends);
+
+		setState(LOADED); //Send signal to use and display the adapter
 	}
 
-	private static class loadFriends extends AsyncTask<Void, Void, Void> {
+	//todo: [feature] FriendsFragment: load the data of best-friends
+	private final class Adapter extends BaseAdapter {
+		private final BlockingQueue<Runnable> executesQueue = new ArrayBlockingQueue<>(15000);
+		private final ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(10, 15, 10, TimeUnit.SECONDS, executesQueue);
+		private final Picasso imageLoader;
+
+		private final LayoutInflater inflater;
+		private final Player[] players;
+
+		Adapter(LayoutInflater inflater, Context context, IgnProvider ignProvider, String[] uuids) {
+			this.inflater = inflater;
+			this.imageLoader = new Picasso.Builder(context).build();
+			this.players = new Player[uuids.length];
+
+			for (int i = 0; i < uuids.length; i++) {
+
+				Player player = (players[i] = new Player());
+				player.uuid = uuids[i];
+
+				poolExecutor.execute(new Runnable() {
+					@Override
+					public void run() {
+						//IgnProvider will either returns null or the result
+						//the username is NULL anyways. So, don't check the value
+						player.username = ignProvider.getUsername(player.uuid, false);
+					}
+				});
+			}
+		}
+
 		@Override
-		protected Void doInBackground(Void... voids) {
+		public int getCount() {
+			return this.players.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
 			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final Holder holder;
+			Player player = this.players[position];
+
+			if (convertView == null) {
+				holder = new Holder();
+				convertView = this.inflater.inflate(R.layout.players_list_adatper_item, null);
+
+				holder.head = convertView.findViewById(R.id.SPL_item_image);
+				holder.text1 = convertView.findViewById(R.id.SPL_item_text1);
+				holder.text2 = convertView.findViewById(R.id.SPL_item_text2);
+
+				convertView.setTag(holder);
+			} else {
+				holder = (Holder) convertView.getTag();
+			}
+
+			holder.text1.setText((player.username != null) ? player.username : player.uuid);
+
+			this.imageLoader
+					.load("https://crafatar.com/avatars/" + player.uuid + "?overlay&default=MHF_Alex")
+					.placeholder(R.drawable.default_player_head)
+					.into(holder.head);
+
+			return convertView;
+		}
+
+		private final class Holder {
+			ImageView head;
+			TextView text1;
+			TextView text2;
+		}
+
+		private final class Player {
+			String uuid;
+			String username;
+
+			//String doing;
+			//boolean isCached;
 		}
 	}
 }
